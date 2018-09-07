2fbbf052d55f483c4df9fd31a837e02f41b67d02
manish <manish@DESKTOP-USI23C7>
2018-09-08 01:15:20 +0530

    driver commit

--- JavaFramework/src/test/java/com/testing/google/TestingClass.java
+++ JavaFramework/src/test/java/com/testing/google/TestingClass.java
@@ -1,5 +1,25 @@
 package com.testing.google;
 
-public class TestingClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
+public class TestingClass {
+	private WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		//Set Driver Path
+		System.setProperty("", "");
+		//launch browser
+		driver = new ChromeDriver();
+	}
+	public void searchTextInGoogle() {
+		
+	}
+	@AfterMethod
+	public void TearDown() {
+		
+	}
 }
