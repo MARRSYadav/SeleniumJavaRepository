b6ba5f11e4052d60a701259ca877e4fdecc339fd
manish <manish@DESKTOP-USI23C7>
2018-09-09 17:23:02 +0530

    committed

--- JavaFramework/src/test/java/com/testing/google/TestingClass.java
+++ JavaFramework/src/test/java/com/testing/google/TestingClass.java
@@ -1,25 +1,15 @@
 package com.testing.google;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
 public class TestingClass {
 	private WebDriver driver;
-	
-	@BeforeMethod
-	public void setup() {
-		//Set Driver Path
-		System.setProperty("", "");
-		//launch browser
-		driver = new ChromeDriver();
-	}
-	public void searchTextInGoogle() {
-		
-	}
-	@AfterMethod
-	public void TearDown() {
-		
+
+	@Test
+	public void launchBrowser() {
+		System.setProperty("webdriver.edge.driver", "./Driver/MicrosoftWebDriver.exe");
+		driver = new EdgeDriver();
 	}
 }
